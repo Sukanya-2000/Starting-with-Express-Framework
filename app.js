117da1d6ea818f4ebc8906a78e189244a078032c
Sukanya-2000 <bhattacharyyasukanya5@gmail.com>
2024-01-21 12:57:03 +0530

    added middleware in first assignment through third commit

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('In another middleware!');
-  res.send('<h1>Hello from Express!</h1>');
+  res.send('<h1>Hello to nodeJS!</h1>');
 });
 
 app.listen(3000);
